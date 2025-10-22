3eb67f843eee203fd6acb4fbdc962b9a5f5f71d7
Alex-DG <alexandre.diguida@gmail.com>
2025-10-22 17:17:55 +0900

    Added coin UI and different coins

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
@@ -5,13 +6,23 @@ public class Coin : MonoBehaviour
 
     public AudioClip coinClip;
 
+    public int coinsToGive = 1;
+
+    private TextMeshProUGUI cointText;
+
+    private void Start()
+    {
+        cointText = GameObject.FindWithTag("CoinText").GetComponent<TextMeshProUGUI>();
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Player player = collision.gameObject.GetComponent<Player>();
-            player.coins += 1;
+            player.coins += coinsToGive;
             player.PlaySFX(coinClip, 0.4f);
+            cointText.text = player.coins.ToString();
             Destroy(gameObject);
         }
     }
